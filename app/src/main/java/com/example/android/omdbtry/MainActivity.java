eb8722424f4689ef12e54fab90d2487ed1ff60c4
DIPTARK BOSE <DIPTARK BOSE>
2016-09-23 19:29:19 +0530

    added api

--- app/src/main/java/com/example/android/omdbtry/MainActivity.java
+++ app/src/main/java/com/example/android/omdbtry/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.android.omdbtry;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText et;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = (EditText) findViewById(R.id.editText);
+        btn=(Button) findViewById(R.id.search);
+        btn.setOnClickListener(this);
     }
-    public void setOn
+
+    @Override
+    public void onClick(View view)
+    {
+        Intent i= new Intent(MainActivity.this, Movie.class);
+        i.putExtra("Movie name", et.getText().toString());
+        startActivity(i);
+    }
+
 }
